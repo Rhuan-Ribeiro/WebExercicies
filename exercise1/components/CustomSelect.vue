a879c11d5d3f8d24c0c7bd15711f2f4349a3c59a
Rhuan Ribeiro <rhuan.augusto.ribeiro@gmail.com>
2024-03-14 22:53:34 -0300

    first commit

---
+++ exercise1/components/CustomSelect.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const props = defineProps({
+  text: {type: String, required: true},
+  options: {type: Object, required: true},
+  value: {type: String, require: true}
+});
+
+const select = defineModel();
+</script>
+
+<template>
+  <div>
+    <select v-model="select">
+      <option v-for="option in props.options" v-bind:value="option.value">
+        {{ option.text }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ exercise1/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>teste</h1>
+    <CustomSelect />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+  const options = [  ];
+</style>
